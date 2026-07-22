7828ae40788fcd1e6695c146f00770b243dc77c1
Miguel Nischor <miguel@nischor.com.br>
2026-07-21 21:23:25 -0400

    feat: add CategoryJpaMapper for mapping between Category and CategoryJpaEntity

---
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/infrastructure/persistence/mapper/CategoryJpaMapper.java
@@ -0,0 +1,17 @@
+package br.com.nischor.ledgerxbackend.accounting.infrastructure.persistence.mapper;
+
+import br.com.nischor.ledgerxbackend.accounting.domain.model.Category;
+import br.com.nischor.ledgerxbackend.accounting.infrastructure.persistence.entity.CategoryJpaEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryJpaMapper {
+
+    public Category toDomain(CategoryJpaEntity entity) {
+        return new Category(entity.getId(), entity.getCompanyId(), entity.getName(), entity.getType());
+    }
+
+    public CategoryJpaEntity toEntity(Category category) {
+        return new CategoryJpaEntity(category.getCompanyId(), category.getName(), category.getType());
+    }
+}
