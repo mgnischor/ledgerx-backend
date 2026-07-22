6ad0a6c926eff5a4cf8c3bc12637faed94ec619c
Miguel Nischor <miguel@nischor.com.br>
2026-07-21 21:23:06 -0400

    feat: add TransactionRepository interface for managing transactions

---
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/domain/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package br.com.nischor.ledgerxbackend.accounting.domain.repository;
+
+import br.com.nischor.ledgerxbackend.accounting.domain.model.Transaction;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public interface TransactionRepository {
+
+    Transaction save(Transaction transaction);
+
+    List<Transaction> findByFinancialAccountIdAndPeriod(UUID financialAccountId, LocalDate from, LocalDate to);
+}
