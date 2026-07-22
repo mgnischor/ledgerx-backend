6a2695f3536d5fb9c6d0605d9c87eb0242c030e1
Miguel Nischor <miguel@nischor.com.br>
2026-07-21 21:26:54 -0400

    feat: create CompanyRepository interface for company data operations

---
+++ src/main/java/br/com/nischor/ledgerxbackend/company/domain/repository/CompanyRepository.java
@@ -0,0 +1,17 @@
+package br.com.nischor.ledgerxbackend.company.domain.repository;
+
+import br.com.nischor.ledgerxbackend.company.domain.model.Company;
+import br.com.nischor.ledgerxbackend.shared.domain.valueobject.DocumentNumber;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CompanyRepository {
+
+    Company save(Company company);
+
+    Optional<Company> findById(UUID id);
+
+    Optional<Company> findByCnpj(DocumentNumber cnpj);
+
+    boolean existsByCnpj(DocumentNumber cnpj);
+}
