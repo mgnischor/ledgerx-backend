2eb7ff372a45232bbf3e120b3ecc0bae5cf6f707
Miguel Nischor <miguel@nischor.com.br>
2026-07-21 21:23:29 -0400

    feat: add FinancialAccountJpaMapper for mapping between FinancialAccount and FinancialAccountJpaEntity

---
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/infrastructure/persistence/mapper/FinancialAccountJpaMapper.java
@@ -0,0 +1,21 @@
+package br.com.nischor.ledgerxbackend.accounting.infrastructure.persistence.mapper;
+
+import br.com.nischor.ledgerxbackend.accounting.domain.model.FinancialAccount;
+import br.com.nischor.ledgerxbackend.accounting.infrastructure.persistence.entity.FinancialAccountJpaEntity;
+import br.com.nischor.ledgerxbackend.shared.domain.valueobject.Money;
+import java.util.Currency;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FinancialAccountJpaMapper {
+
+    public FinancialAccount toDomain(FinancialAccountJpaEntity entity) {
+        var balance = new Money(entity.getBalance(), Currency.getInstance(entity.getCurrencyCode()));
+        return new FinancialAccount(entity.getId(), entity.getCompanyId(), entity.getName(), balance);
+    }
+
+    public FinancialAccountJpaEntity toEntity(FinancialAccount account) {
+        return new FinancialAccountJpaEntity(account.getCompanyId(), account.getName(),
+                account.getBalance().amount(), account.getBalance().currency().getCurrencyCode());
+    }
+}
