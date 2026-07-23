f67ac12dacef4ef29c42c3262136ba815f0a88fb
Miguel Nischor <miguel@nischor.com.br>
2026-07-22 20:02:29 -0400

    feat: add ValidCnpj annotation for validating Brazilian CNPJ

---
+++ src/main/java/br/com/nischor/ledgerxbackend/shared/infrastructure/validation/ValidCnpj.java
@@ -0,0 +1,20 @@
+package br.com.nischor.ledgerxbackend.shared.infrastructure.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CnpjValidator.class)
+public @interface ValidCnpj {
+
+    String message() default "invalid CNPJ";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
