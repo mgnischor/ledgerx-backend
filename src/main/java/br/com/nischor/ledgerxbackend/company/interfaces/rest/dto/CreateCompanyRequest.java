fa59d1178970356b8440dacdb3f790dd24a75c54
Miguel Nischor <miguel@nischor.com.br>
2026-07-22 20:00:48 -0400

    feat: add validation annotations to CreateCompanyRequest DTO

--- src/main/java/br/com/nischor/ledgerxbackend/company/interfaces/rest/dto/CreateCompanyRequest.java
+++ src/main/java/br/com/nischor/ledgerxbackend/company/interfaces/rest/dto/CreateCompanyRequest.java
@@ -1,17 +1,22 @@
 package br.com.nischor.ledgerxbackend.company.interfaces.rest.dto;
 
 import br.com.nischor.ledgerxbackend.company.domain.model.CompanySize;
+import br.com.nischor.ledgerxbackend.shared.infrastructure.validation.ValidBrazilianState;
+import br.com.nischor.ledgerxbackend.shared.infrastructure.validation.ValidBrazilianZipCode;
+import br.com.nischor.ledgerxbackend.shared.infrastructure.validation.ValidCnpj;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateCompanyRequest(
-        @NotBlank String legalName,
-        @NotBlank String tradeName,
-        @NotBlank String cnpj,
-        CompanySize size,
-        @NotBlank String street,
-        @NotBlank String number,
-        @NotBlank String city,
-        @NotBlank String state,
-        @NotBlank String zipCode,
-        @NotBlank String country) {
+        @NotBlank @Size(max = 150) String legalName,
+        @NotBlank @Size(max = 150) String tradeName,
+        @NotBlank @ValidCnpj String cnpj,
+        @NotNull CompanySize size,
+        @NotBlank @Size(max = 150) String street,
+        @NotBlank @Size(max = 20) String number,
+        @NotBlank @Size(max = 100) String city,
+        @NotBlank @ValidBrazilianState String state,
+        @NotBlank @ValidBrazilianZipCode String zipCode,
+        @NotBlank @Size(max = 60) String country) {
 }
