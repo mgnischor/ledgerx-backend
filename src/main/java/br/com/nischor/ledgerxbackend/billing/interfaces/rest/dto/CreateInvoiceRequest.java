ef28d8081772889437548d95c5467666b6250b86
Miguel Nischor <miguel@nischor.com.br>
2026-07-22 20:00:28 -0400

    feat: add validation for installment amounts and due date in CreateInvoiceRequest

--- src/main/java/br/com/nischor/ledgerxbackend/billing/interfaces/rest/dto/CreateInvoiceRequest.java
+++ src/main/java/br/com/nischor/ledgerxbackend/billing/interfaces/rest/dto/CreateInvoiceRequest.java
@@ -1,8 +1,10 @@
 package br.com.nischor.ledgerxbackend.billing.interfaces.rest.dto;
 
 import br.com.nischor.ledgerxbackend.billing.domain.model.PartyType;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +14,6 @@ public record CreateInvoiceRequest(
         @NotNull UUID companyId,
         @NotNull UUID partyId,
         @NotNull PartyType direction,
-        @NotEmpty List<BigDecimal> installmentAmounts,
-        @NotNull LocalDate firstDueDate) {
+        @NotEmpty @Size(max = 60) List<BigDecimal> installmentAmounts,
+        @NotNull @FutureOrPresent LocalDate firstDueDate) {
 }
