c4932dd7e2e6de0d54637d71dbcf5a73407ea6cf
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:25 -0400

    fix: enhance documentation for EntityNotFoundException

--- src/main/java/br/com/nischor/ledgerxbackend/shared/domain/exception/EntityNotFoundException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/shared/domain/exception/EntityNotFoundException.java
@@ -1,7 +1,16 @@
 package br.com.nischor.ledgerxbackend.shared.domain.exception;
 
+/**
+ * Thrown when an entity of a given type cannot be found for a given identifier.
+ */
 public class EntityNotFoundException extends DomainException {
 
+    /**
+     * Creates a new exception describing the missing entity.
+     *
+     * @param entityType the class of the entity that could not be found
+     * @param id         the identifier that was used to look up the entity
+     */
     public EntityNotFoundException(Class<?> entityType, Object id) {
         super("%s not found for id %s".formatted(entityType.getSimpleName(), id));
     }
