993583d51db579c324f083f7907e92691139b5fd
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:05 -0400

    fix: enhance documentation for PartyType

--- src/main/java/br/com/nischor/ledgerxbackend/billing/domain/model/PartyType.java
+++ src/main/java/br/com/nischor/ledgerxbackend/billing/domain/model/PartyType.java
@@ -1,6 +1,11 @@
 package br.com.nischor.ledgerxbackend.billing.domain.model;
 
+/**
+ * Represents the role a {@link Party} plays in a transaction, and the direction of the associated invoice.
+ */
 public enum PartyType {
+    /** The party is a customer; invoices of this type are issued to the party. */
     CUSTOMER,
+    /** The party is a supplier; invoices of this type are received from the party. */
     SUPPLIER
 }
