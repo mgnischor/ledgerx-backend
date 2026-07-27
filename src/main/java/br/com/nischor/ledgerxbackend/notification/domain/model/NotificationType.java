5b1ddfabee6c345e2b6947a2a580c31c19bac7f8
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:22 -0400

    fix: enhance documentation for NotificationType

--- src/main/java/br/com/nischor/ledgerxbackend/notification/domain/model/NotificationType.java
+++ src/main/java/br/com/nischor/ledgerxbackend/notification/domain/model/NotificationType.java
@@ -1,7 +1,13 @@
 package br.com.nischor.ledgerxbackend.notification.domain.model;
 
+/**
+ * Categories of domain events that can trigger the creation of a {@link Notification}.
+ */
 public enum NotificationType {
+    /** A new user has registered. */
     USER_REGISTERED,
+    /** A transaction has been recorded. */
     TRANSACTION_RECORDED,
+    /** An invoice has been paid. */
     INVOICE_PAID
 }
