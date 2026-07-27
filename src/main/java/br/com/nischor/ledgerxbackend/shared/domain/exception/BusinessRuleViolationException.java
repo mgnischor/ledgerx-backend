62274fed76e137790b28e82f535554972836a57b
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:24 -0400

    fix: enhance documentation for BusinessRuleViolationException

--- src/main/java/br/com/nischor/ledgerxbackend/shared/domain/exception/BusinessRuleViolationException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/shared/domain/exception/BusinessRuleViolationException.java
@@ -1,7 +1,16 @@
 package br.com.nischor.ledgerxbackend.shared.domain.exception;
 
+/**
+ * Thrown when an operation violates a business rule enforced by the domain
+ * layer (e.g. an invariant that must hold for an aggregate or value object).
+ */
 public class BusinessRuleViolationException extends DomainException {
 
+    /**
+     * Creates a new exception describing the violated business rule.
+     *
+     * @param message a human-readable description of the violated rule
+     */
     public BusinessRuleViolationException(String message) {
         super(message);
     }
