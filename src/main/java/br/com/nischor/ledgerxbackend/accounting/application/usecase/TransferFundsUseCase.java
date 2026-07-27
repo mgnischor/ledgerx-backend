4cf446e3109eac0450688995829713bb030694a9
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:34:50 -0400

    fix: enhance documentation for TransferFundsUseCase and its execute method

--- src/main/java/br/com/nischor/ledgerxbackend/accounting/application/usecase/TransferFundsUseCase.java
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/application/usecase/TransferFundsUseCase.java
@@ -8,15 +8,34 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Transfers funds between two financial accounts by debiting the source account and crediting the destination
+ * account.
+ */
 @Service
 public class TransferFundsUseCase {
 
     private final FinancialAccountRepository financialAccountRepository;
 
+    /**
+     * Creates the use case.
+     *
+     * @param financialAccountRepository repository used to look up and persist the involved accounts
+     */
     public TransferFundsUseCase(FinancialAccountRepository financialAccountRepository) {
         this.financialAccountRepository = financialAccountRepository;
     }
 
+    /**
+     * Transfers the given amount from one financial account to another.
+     *
+     * @param fromAccountId the identifier of the account funds are debited from
+     * @param toAccountId the identifier of the account funds are credited to
+     * @param amount the amount to transfer
+     * @throws EntityNotFoundException if either account does not exist
+     * @throws br.com.nischor.ledgerxbackend.accounting.domain.exception.InsufficientBalanceException if the
+     *         source account does not have enough balance to cover the transfer
+     */
     @Transactional
     public void execute(UUID fromAccountId, UUID toAccountId, Money amount) {
         var source = financialAccountRepository.findById(fromAccountId)
