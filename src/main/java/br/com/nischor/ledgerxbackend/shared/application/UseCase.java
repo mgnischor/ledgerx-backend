b4541a0061441428d2885f764b10fd3b69061e4b
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:24 -0400

    fix: enhance documentation for UseCase

--- src/main/java/br/com/nischor/ledgerxbackend/shared/application/UseCase.java
+++ src/main/java/br/com/nischor/ledgerxbackend/shared/application/UseCase.java
@@ -1,6 +1,22 @@
 package br.com.nischor.ledgerxbackend.shared.application;
 
+/**
+ * Generic contract for an application use case in the hexagonal architecture.
+ *
+ * <p>Implementations encapsulate a single application-level operation, receiving
+ * an input and producing an output, keeping application logic decoupled from
+ * inbound adapters (controllers, messaging listeners, etc.).
+ *
+ * @param <INPUT>  the type of the input required to execute the use case
+ * @param <OUTPUT> the type of the result produced by the use case
+ */
 public interface UseCase<INPUT, OUTPUT> {
 
+    /**
+     * Executes the use case with the given input.
+     *
+     * @param input the input data required to perform the operation
+     * @return the result produced by executing the use case
+     */
     OUTPUT execute(INPUT input);
 }
