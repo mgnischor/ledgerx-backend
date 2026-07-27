5be19d8833f02e3f5821e889923245d359123d74
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:14 -0400

    fix: enhance documentation for CreateCompanyRequest

--- src/main/java/br/com/nischor/ledgerxbackend/company/interfaces/rest/dto/CreateCompanyRequest.java
+++ src/main/java/br/com/nischor/ledgerxbackend/company/interfaces/rest/dto/CreateCompanyRequest.java
@@ -8,6 +8,21 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+/**
+ * REST request payload for registering a new company, carrying its identification, size and
+ * address data with bean validation constraints.
+ *
+ * @param legalName registered legal name of the company
+ * @param tradeName trade (fantasy) name of the company
+ * @param cnpj Brazilian CNPJ document number, validated by {@link ValidCnpj}
+ * @param size company size classification
+ * @param street street name of the company's address
+ * @param number street number of the company's address
+ * @param city city of the company's address
+ * @param state Brazilian state (UF) of the company's address, validated by {@link ValidBrazilianState}
+ * @param zipCode postal code (CEP) of the company's address, validated by {@link ValidBrazilianZipCode}
+ * @param country country of the company's address
+ */
 public record CreateCompanyRequest(
         @NotBlank @Size(max = 150) String legalName,
         @NotBlank @Size(max = 150) String tradeName,
