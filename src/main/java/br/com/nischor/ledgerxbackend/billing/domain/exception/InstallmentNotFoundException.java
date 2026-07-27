fba20de7c934c82ac8f433c207b3b517c194df48
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:04 -0400

    fix: enhance documentation for InstallmentNotFoundException

--- src/main/java/br/com/nischor/ledgerxbackend/billing/domain/exception/InstallmentNotFoundException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/billing/domain/exception/InstallmentNotFoundException.java
@@ -2,8 +2,16 @@ package br.com.nischor.ledgerxbackend.billing.domain.exception;
 
 import br.com.nischor.ledgerxbackend.shared.domain.exception.BusinessRuleViolationException;
 
+/**
+ * Thrown when an installment cannot be found by its identifier.
+ */
 public class InstallmentNotFoundException extends BusinessRuleViolationException {
 
+    /**
+     * Creates the exception with a message referencing the missing installment.
+     *
+     * @param installmentId the identifier of the installment that could not be found.
+     */
     public InstallmentNotFoundException(String installmentId) {
         super("Installment not found: %s".formatted(installmentId));
     }
