4912151e24e83dc5b14fbf7f7454946afa90facd
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:20 -0400

    fix: enhance documentation for GrantRoleRequest

--- src/main/java/br/com/nischor/ledgerxbackend/identity/interfaces/rest/dto/GrantRoleRequest.java
+++ src/main/java/br/com/nischor/ledgerxbackend/identity/interfaces/rest/dto/GrantRoleRequest.java
@@ -3,5 +3,10 @@ package br.com.nischor.ledgerxbackend.identity.interfaces.rest.dto;
 import br.com.nischor.ledgerxbackend.identity.domain.model.Role;
 import jakarta.validation.constraints.NotNull;
 
+/**
+ * Request payload for granting a role to an existing user.
+ *
+ * @param role the role to grant; must be a valid, non-null {@link Role}.
+ */
 public record GrantRoleRequest(@NotNull Role role) {
 }
