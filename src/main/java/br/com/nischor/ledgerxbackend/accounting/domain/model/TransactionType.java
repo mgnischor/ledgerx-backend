36a1ce60d1f2b83fa8ad2620414530ca4bf1c161
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:34:51 -0400

    fix: enhance documentation for TransactionType

--- src/main/java/br/com/nischor/ledgerxbackend/accounting/domain/model/TransactionType.java
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/domain/model/TransactionType.java
@@ -1,7 +1,13 @@
 package br.com.nischor.ledgerxbackend.accounting.domain.model;
 
+/**
+ * Classifies the nature of a financial transaction or category.
+ */
 public enum TransactionType {
+    /** Money coming into an account. */
     INCOME,
+    /** Money going out of an account. */
     EXPENSE,
+    /** Money moved between two accounts. */
     TRANSFER
 }
