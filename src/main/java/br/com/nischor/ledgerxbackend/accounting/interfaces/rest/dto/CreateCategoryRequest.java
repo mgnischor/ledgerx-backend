bc04dafc23abf3a5be447ca8eea5783744093c61
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:02 -0400

    fix: enhance documentation for CreateCategoryRequest

--- src/main/java/br/com/nischor/ledgerxbackend/accounting/interfaces/rest/dto/CreateCategoryRequest.java
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/interfaces/rest/dto/CreateCategoryRequest.java
@@ -5,5 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+/**
+ * REST request payload for creating a category.
+ *
+ * @param name the category name; required, up to 60 characters
+ * @param type the transaction type (income, expense, or transfer) the category is associated with
+ */
 public record CreateCategoryRequest(@NotBlank @Size(max = 60) String name, @NotNull TransactionType type) {
 }
