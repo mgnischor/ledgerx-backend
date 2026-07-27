5c0dcc0970586953aa34f88948df2d8f5e0cdc50
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:16 -0400

    fix: enhance documentation for InvalidCredentialsException

--- src/main/java/br/com/nischor/ledgerxbackend/identity/domain/exception/InvalidCredentialsException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/identity/domain/exception/InvalidCredentialsException.java
@@ -2,8 +2,16 @@ package br.com.nischor.ledgerxbackend.identity.domain.exception;
 
 import br.com.nischor.ledgerxbackend.shared.domain.exception.DomainException;
 
+/**
+ * Thrown when a login attempt fails because the supplied email or password does not match an
+ * active user account.
+ */
 public class InvalidCredentialsException extends DomainException {
 
+    /**
+     * Creates the exception with a generic message that does not disclose whether the email or
+     * the password was the cause of the failure.
+     */
     public InvalidCredentialsException() {
         super("Invalid email or password");
     }
