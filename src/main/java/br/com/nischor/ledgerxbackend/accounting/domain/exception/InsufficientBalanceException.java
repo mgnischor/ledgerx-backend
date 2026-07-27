4f7d8d96b6f1d4c2881acb830ad3f985e1b5a764
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:34:50 -0400

    fix: enhance documentation for InsufficientBalanceException

--- src/main/java/br/com/nischor/ledgerxbackend/accounting/domain/exception/InsufficientBalanceException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/domain/exception/InsufficientBalanceException.java
@@ -2,8 +2,16 @@ package br.com.nischor.ledgerxbackend.accounting.domain.exception;
 
 import br.com.nischor.ledgerxbackend.shared.domain.exception.BusinessRuleViolationException;
 
+/**
+ * Thrown when an operation would require a financial account to hold a balance greater than it currently has.
+ */
 public class InsufficientBalanceException extends BusinessRuleViolationException {
 
+    /**
+     * Creates the exception with a message identifying the affected account.
+     *
+     * @param accountName the name of the account that has insufficient balance
+     */
     public InsufficientBalanceException(String accountName) {
         super("Account '%s' has insufficient balance for this operation".formatted(accountName));
     }
