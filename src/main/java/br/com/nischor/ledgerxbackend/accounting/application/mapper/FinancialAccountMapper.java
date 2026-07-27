7dcad2f5a8eeecb065c817f2a10e535eec2f44ac
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:29:56 -0400

    fix: enhance documentation for FinancialAccountMapper and its toDto method

--- src/main/java/br/com/nischor/ledgerxbackend/accounting/application/mapper/FinancialAccountMapper.java
+++ src/main/java/br/com/nischor/ledgerxbackend/accounting/application/mapper/FinancialAccountMapper.java
@@ -4,9 +4,19 @@ import br.com.nischor.ledgerxbackend.accounting.application.dto.FinancialAccount
 import br.com.nischor.ledgerxbackend.accounting.domain.model.FinancialAccount;
 import org.springframework.stereotype.Component;
 
+/**
+ * Converts {@link FinancialAccount} domain objects into {@link FinancialAccountDto} instances for use by the
+ * application layer.
+ */
 @Component
 public class FinancialAccountMapper {
 
+    /**
+     * Converts a {@link FinancialAccount} domain object into its DTO representation.
+     *
+     * @param account the domain financial account to convert
+     * @return the resulting {@link FinancialAccountDto}
+     */
     public FinancialAccountDto toDto(FinancialAccount account) {
         return new FinancialAccountDto(account.getId(), account.getCompanyId(), account.getName(),
                 account.getBalance().amount(), account.getBalance().currency().getCurrencyCode(),
