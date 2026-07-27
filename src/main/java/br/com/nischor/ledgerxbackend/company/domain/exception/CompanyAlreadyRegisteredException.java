f6c09942494d737c08716ba358efc00751c68f3d
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:12 -0400

    fix: enhance documentation for CompanyAlreadyRegisteredException

--- src/main/java/br/com/nischor/ledgerxbackend/company/domain/exception/CompanyAlreadyRegisteredException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/company/domain/exception/CompanyAlreadyRegisteredException.java
@@ -2,8 +2,16 @@ package br.com.nischor.ledgerxbackend.company.domain.exception;
 
 import br.com.nischor.ledgerxbackend.shared.domain.exception.BusinessRuleViolationException;
 
+/**
+ * Thrown when attempting to register a company whose CNPJ is already registered.
+ */
 public class CompanyAlreadyRegisteredException extends BusinessRuleViolationException {
 
+    /**
+     * Creates the exception with a message identifying the conflicting CNPJ.
+     *
+     * @param cnpj the CNPJ that is already registered
+     */
     public CompanyAlreadyRegisteredException(String cnpj) {
         super("Company already registered for CNPJ: %s".formatted(cnpj));
     }
