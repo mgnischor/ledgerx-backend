7ef91a709406e7c6c76318f237743de71f5788ab
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:15 -0400

    fix: enhance documentation for UserMapper and its toDto method

--- src/main/java/br/com/nischor/ledgerxbackend/identity/application/mapper/UserMapper.java
+++ src/main/java/br/com/nischor/ledgerxbackend/identity/application/mapper/UserMapper.java
@@ -4,9 +4,19 @@ import br.com.nischor.ledgerxbackend.identity.application.dto.UserDto;
 import br.com.nischor.ledgerxbackend.identity.domain.model.User;
 import org.springframework.stereotype.Component;
 
+/**
+ * Converts between the {@link User} domain model and its outward-facing {@link UserDto}
+ * representation.
+ */
 @Component
 public class UserMapper {
 
+    /**
+     * Converts a domain {@link User} into a {@link UserDto}.
+     *
+     * @param user the domain user to convert.
+     * @return the corresponding data transfer object.
+     */
     public UserDto toDto(User user) {
         return new UserDto(user.getId(), user.getFullName(), user.getEmail().value(), user.getRoles(),
                 user.isActive());
