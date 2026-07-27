fc9b18a9f5342c60a257a8f724cc451659632af8
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:12 -0400

    fix: enhance documentation for Address

--- src/main/java/br/com/nischor/ledgerxbackend/company/domain/valueobject/Address.java
+++ src/main/java/br/com/nischor/ledgerxbackend/company/domain/valueobject/Address.java
@@ -1,4 +1,14 @@
 package br.com.nischor.ledgerxbackend.company.domain.valueobject;
 
+/**
+ * Immutable value object representing a postal address.
+ *
+ * @param street street name
+ * @param number street number
+ * @param city city name
+ * @param state state or federative unit (UF)
+ * @param zipCode postal code (CEP)
+ * @param country country name
+ */
 public record Address(String street, String number, String city, String state, String zipCode, String country) {
 }
