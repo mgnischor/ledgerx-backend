50eb4d7bd443e2254060ba6ba7755d481f7fffbd
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:12 -0400

    fix: enhance documentation for CompanySize

--- src/main/java/br/com/nischor/ledgerxbackend/company/domain/model/CompanySize.java
+++ src/main/java/br/com/nischor/ledgerxbackend/company/domain/model/CompanySize.java
@@ -1,7 +1,13 @@
 package br.com.nischor.ledgerxbackend.company.domain.model;
 
+/**
+ * Classifies a company by its size according to Brazilian business categories.
+ */
 public enum CompanySize {
+    /** Individual micro-entrepreneur (Microempreendedor Individual). */
     MEI,
+    /** Microenterprise (Microempresa). */
     MICRO,
+    /** Small business (Empresa de Pequeno Porte). */
     SMALL
 }
