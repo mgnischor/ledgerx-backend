c30f48160bcde088daafd06612e94c6b06e64059
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 21:35:16 -0400

    fix: enhance documentation for EmailAlreadyRegisteredException

--- src/main/java/br/com/nischor/ledgerxbackend/identity/domain/exception/EmailAlreadyRegisteredException.java
+++ src/main/java/br/com/nischor/ledgerxbackend/identity/domain/exception/EmailAlreadyRegisteredException.java
@@ -2,8 +2,16 @@ package br.com.nischor.ledgerxbackend.identity.domain.exception;
 
 import br.com.nischor.ledgerxbackend.shared.domain.exception.BusinessRuleViolationException;
 
+/**
+ * Thrown when an attempt is made to register a user with an email address that is already in use.
+ */
 public class EmailAlreadyRegisteredException extends BusinessRuleViolationException {
 
+    /**
+     * Creates the exception for the given email address.
+     *
+     * @param email the email address that is already registered.
+     */
     public EmailAlreadyRegisteredException(String email) {
         super("Email already registered: %s".formatted(email));
     }
